914336a8ce24c0baec4384dd9b8f6cc7cf9bbc11
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2016-06-04 02:07:59 +0200

    Ensure /var/lib/puppet is owned by user puppet
    If /var/lib/puppet is a host volume, by default it will be owned by root, and puppetserver will fail to start.

--- scripts/puppetserver.sh
+++ scripts/puppetserver.sh
@@ -12,6 +12,9 @@ SERVICE_STOP_RETRIES=60
 # ExecStartPre
 /usr/bin/install --directory --owner=puppet --group=puppet --mode=775 /var/run/puppetlabs/puppetserver
 
+# set ownership on var directory
+chown puppet /var/lib/puppet
+
 # copied from SystemD unit provided by puppet server
 exec sudo -u puppet -H /usr/bin/java $JAVA_ARGS '-XX:OnOutOfMemoryError=kill -9 %%p' -Djava.security.egd=/dev/urandom \
     -cp "${INSTALL_DIR}/puppet-server-release.jar" clojure.main \
