daee2a398e2d2ebd156764a01046a7f05a93e434
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2016-06-16 16:57:59 -0700

    JSON Logging for Puppet Server 1.1.3.

--- scripts/puppetserver.sh
+++ scripts/puppetserver.sh
@@ -9,12 +9,13 @@ CONFIG="${CONFIG:-/etc/puppetserver/conf.d}"
 BOOTSTRAP_CONFIG="${BOOTSTRAP_CONFIG:-/etc/puppetserver/bootstrap.cfg}"
 SERVICE_STOP_RETRIES=60
 
-# ExecStartPre
-/usr/bin/install --directory --owner=puppet --group=puppet --mode=775 /var/run/puppetlabs/puppetserver
-
 # copied from SystemD unit provided by puppet server
-exec sudo -u puppet -H /usr/bin/java $JAVA_ARGS '-XX:OnOutOfMemoryError=kill -9 %%p' -Djava.security.egd=/dev/urandom \
-    -cp "${INSTALL_DIR}/puppet-server-release.jar" clojure.main \
-    -m puppetlabs.trapperkeeper.main \
-    --config "${CONFIG}" \
-    -b "${BOOTSTRAP_CONFIG}" $@
+exec /usr/bin/java $JAVA_ARGS \
+          '-XX:OnOutOfMemoryError=kill -9 %%p' \
+          -XX:+HeapDumpOnOutOfMemoryError \
+          -XX:HeapDumpPath=/tmp \
+          -Djava.security.egd=/dev/urandom \
+          -cp "${INSTALL_DIR}/puppet-server-release.jar" clojure.main \
+          -m puppetlabs.trapperkeeper.main \
+          --config "${CONFIG}" \
+          -b "${BOOTSTRAP_CONFIG}" $@
