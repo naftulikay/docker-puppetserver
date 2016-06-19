03d32d91544029d38b3b6765820d48d927b35627
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2016-06-19 15:57:01 -0700

    Conditional configuration restoration.
    Closes #5.
    
    If configuration files aren't present in specific config
    directories on container startup, they will be restored from
    backup.
    
    It is necessary to create the host mount points ahead of time and
    have them owned 8140:8140.

--- scripts/puppetserver.sh
+++ scripts/puppetserver.sh
@@ -2,6 +2,30 @@
 
 set -e
 
+# define functions
+function restore-backup-files() {
+    source_dir="$1"
+    dest_dir="$2"
+
+    # create directories if they don't exist
+    find $source_dir -mindepth 1 -type d | sort | while read d ; do
+        trimmed_d="$(echo $d | sed "s:$source_dir/::g")"
+        test -d "$dest_dir/$trimmed_d" || mkdir -p -m 0755 "$dest_dir/$trimmed_d"
+    done
+
+    # create files if they don't exist
+    find $source_dir -mindepth 1 -type f | sort | while read f ; do
+        trimmed_f="$(echo $f | sed "s:$source_dir/::g")"
+        test -f "$dest_dir/$trimmed_f" || ( cat "$f" > "$dest_dir/$trimmed_f" && \
+             chmod 0644 "$dest_dir/$trimmed_f" )
+    done
+}
+
+# restore default config files if they don't exist
+restore-backup-files /usr/share/puppet/backup/etc /etc/puppet/
+restore-backup-files /usr/share/puppetserver/backup/etc /etc/puppetserver/
+
+# define defaults for environment variables
 JAVA_ARGS="${JAVA_ARGS:--Xms2g -Xmx2g -XX:MaxPermSize=256m}"
 
 INSTALL_DIR="/usr/share/puppetserver"
